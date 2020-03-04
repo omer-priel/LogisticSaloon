65232723511a22b0a80a053f73c6d9d9136a8455
Omer Priel <omer2079@gmail.com>
2020-03-04 23:09:46 +0200

    map fixed

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,23 +1,17 @@
 <template>
     <div id="Map">
-        <HereMapMarker 
-          appId="FymdsOx1OInDIkWIBErG"
-          appCode="i_LyBjYPh3K-ymfzPrpBPw"
-          :lng="37.7397" 
-          :lat="-121.4252"
-          :zoom="14"
-          />
+        <HereMap />
     </div>
 </template>
 
 <script>
-    import HereMapMarker from "./HereMapMarker.vue"
+    import HereMap from "./HereMap.vue"
 
     export default {
         name: 'Map',
 
         components: {
-            HereMapMarker
+            HereMap
         },
 
         data() {
---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export default {
+    HERE_MAP_APP_ID: "FymdsOx1OInDIkWIBErG",
+    HERE_MAP_APP_CODE: "i_LyBjYPh3K-ymfzPrpBPw",
+}
