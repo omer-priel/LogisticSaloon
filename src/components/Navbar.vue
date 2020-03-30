7a8b1768e4fea33b6bbb54a1d6aa30a8fbbfaaf5
Omer Priel <omer2079@gmail.com>
2020-03-30 17:40:44 +0300

    fix texts

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,13 +4,13 @@
 			<b-navbar-nav></b-navbar-nav>
 
 			<b-navbar-nav class="ml-auto">
-				<b-dropdown variant="primary" @click="eventSortByTerritorials()" class="mr-5" text="לפיקוד" split right>
+				<b-dropdown variant="primary" @click="eventSortByTerritorials()" class="mr-5" text="לפי פיקוד" split right>
     				<b-dropdown-item-button @click="eventFilterTerritorial(0)">פצ"ן</b-dropdown-item-button>
     				<b-dropdown-item-button @click="eventFilterTerritorial(1)">פקמ"ז</b-dropdown-item-button>
     				<b-dropdown-item-button @click="eventFilterTerritorial(2)">פד"ם</b-dropdown-item-button>
     			</b-dropdown>
 				
-				<b-dropdown variant="primary" @click="eventSortByEventTypes()" class="mr-5" text="למערך" split right>
+				<b-dropdown variant="primary" @click="eventSortByEventTypes()" class="mr-5" text="לפי מערך" split right>
     				<b-dropdown-item-button v-for="type in types.values()" :key="type" @click="eventFilterEventType(type)" class="text-right">{{ type }}</b-dropdown-item-button>
     			</b-dropdown>
 
